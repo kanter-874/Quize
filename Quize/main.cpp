616b1193acdd007076d20f22217185f236aae24c
kanter-874 <kannosuke55@gmail.com>
2024-10-15 18:59:31 +0900

    サイコロ問題を追加したよ💛

--- Quize/main.cpp
+++ Quize/main.cpp
@@ -11,6 +11,19 @@ struct Question
 	string a;	  //答え
 };
 
+//最大公約数を求める
+int gcd(int a, int b)
+{
+	//余りが0になったときの除数を返す
+	while (b)
+	{
+		int r = a % b;
+		a = b; //除数を次の被除数にする
+		b = r; //あまりを次の除数にする
+	}
+	return a;
+}
+
 int main()
 {
 	vector<Question> questions(3);
@@ -55,6 +68,13 @@ int main()
 	double sphere_volume = static_cast<double>(sphere_radius * sphere_radius * sphere_radius * 4) / 3.0; // 体積
 	questions.push_back({ "半径 " + to_string(sphere_radius) + " cm の球がある。\nこの球の体積を Xπ cm³ とする。X の値を求めよ。\n", to_string(sphere_volume) });
 
+	//サイコロの確率
+	x = uniform_int_distribution<>(1, 5)(rand);
+	y = uniform_int_distribution<>(1, 6 - x)(rand);
+	z = gcd(y + 1, 6);
+	questions.push_back({
+		"サイコロを１こ振って、" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。",
+		to_string((y + 1) / z) + "/" + to_string(6 / z) });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
