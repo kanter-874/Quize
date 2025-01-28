e0979827aaea95e3e40aebf5865ec7e679755f8e
kanter-874 <kannosuke55@gmail.com>
2025-01-28 17:19:14 +0900

    経済の問題が出る用になったよ💛

--- Quize/main.cpp
+++ Quize/main.cpp
@@ -5,12 +5,12 @@
 #include "exam_geography.h"
 #include "exam_politics.h"
 #include "utility.h"
+#include "exam_economics.h"
 #include<iostream>
 #include<string>
 #include<vector>
 #include<random>
 using namespace std;
-
 
 
 //最大公約数を求める
@@ -112,7 +112,7 @@ int main()
 
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=物理\n5=地理\n6=政治\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n4=物理\n5=地理\n6=政治\n7=経済\n";
 	int subject;
 	cin >> subject;
 	if (subject == 2)
@@ -150,29 +150,59 @@ int main()
 		questions = CreatePoliticsExam();
 	}
 
+	else if (subject == 7)
+	{
+		questions = CreateEconomicsExam();
+	}
+
 	for (const auto& e : questions)
 	{
 		cout << e.q << "\n";
 		string answer;
 		cin >> answer;
 
-    // 入力された答えをSJISからASCIIに変換する
-    const string ascii = ConvertSjisNumberToAscii(answer);
-    // 変換が成功した場合はASCII文字列に置き換える
-    if ( ! ascii.empty()) {
-      answer = ascii;
-    }
+		// 入力された答えをSJISからASCIIに変換する
+		const string ascii = ConvertSjisNumberToAscii(answer);
+		// 変換が成功した場合はASCII文字列に置き換える
+		if (!ascii.empty()) {
+			answer = ascii;
+		}
 
 		if (answer == e.a)
 		{
 			cout << "正解!\n";
 			score++;
 		}
-		else
+
+		else if (e.b.empty())
 		{
+			// 答えがひとつだけの場合
 			cout << "間違い！正解は" << e.a << "\n";
 			miss++;
 		}
+		else
+		{
+			// 答えが複数ある場合、いずれかと一致すれば正解とする
+			bool isMatch = false;
+			for (const auto& b : e.b) {
+				if (answer == b) {
+					isMatch = true; // 一致する答えが見つかった
+					break;
+				}
+			}
+
+			// 比較結果を出力
+			if (isMatch) {
+				cout << "正解！\n";
+			}
+			else {
+				cout << "間違い！　正解は" << e.a << "(または";
+				for (auto& b : e.b) {
+					cout << "、" << b;
+				}
+				cout << ")\n";
+			}
+		} // if answer == e.a
 	}//for questions
 
 	if ((score + miss) / 3 <= 10)
